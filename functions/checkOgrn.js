71c3655ff758563e30391352a4da1a7379ddd3ca
Kirill Matrosov <kdmatrosov@avito.ru>
2021-08-02 16:13:34 +0300

    chore(*): upgrade jest dep & add test for ogrnip

--- functions/checkOgrn.js
+++ functions/checkOgrn.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = function (value) {
-    //преобразуем в строку
-    value = "" + value;
-    if (!(/^\d{13}$/.test(value))) {
-      return false;
-    }
-	return parseInt((value.slice(0, -1) % 11).toString().slice(-1)) === +value[12];
+  //преобразуем в строку
+  value = "" + value;
+  if (!(/^\d{13}$/.test(value))) {
+    return false;
+  }
+  return parseInt((value.slice(0, -1) % 11).toString().slice(-1)) === +value[12];
 };
--- functions/checkOgrnip.js
+++ functions/checkOgrnip.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = function (value) {
-    //преобразуем в строку
-    value = "" + value;
-    if (!(/^\d{15}$/.test(value))) {
-      return false;
-    }
-	return parseInt((value.slice(0, -1) % 13).toString().slice(-1)) === +value[14];
+  //преобразуем в строку
+  value = "" + value;
+  if (!(/^\d{15}$/.test(value))) {
+    return false;
+  }
+  return parseInt((value.slice(0, -1) % 13).toString().slice(-1)) === +value[14];
 };
